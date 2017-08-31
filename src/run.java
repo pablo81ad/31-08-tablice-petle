725a3d27ca4c2db848eef645bd3cd41d82df9fdf
pablo81ad <ludwiczek200@wp.pl>
2017-08-31 17:35:24 +0200

    tablice

---
+++ src/run.java
@@ -0,0 +1,28 @@
+/**
+ * Created by ppiekutowski on 31.08.2017.
+ */
+public class run {
+    public static void main(String[] args) {
+        String a[] = new String[10];
+        a[0]="*";
+        a[1]="**";
+        a[2]="***";
+        a[3]="****";
+        a[4]="*****";
+        a[5]="*******";
+        a[6]="********";
+        a[7]="*********";
+        a[8]="**********";
+        a[9]="***********";
+
+
+
+        for (int i=0; i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+        int tab1[]
+
+    }
+}
